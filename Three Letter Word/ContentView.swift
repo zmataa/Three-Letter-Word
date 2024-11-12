e04cb0d94651bf90458be04f9b39cd17dc538895
Zane <zmatarieh@bsd220.org>
2024-11-12 10:06:01 -0600

    Added custom letter box struct

--- Three Letter Word/ContentView.swift
+++ Three Letter Word/ContentView.swift
@@ -8,17 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var letter = ""
+    @State private var counter = 0
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Three Letter Word")
+                .font(.title).bold()
+                .padding()
+            Text("Tap the gray box to change the letter")
         }
-        .padding()
     }
 }
 
+struct CustomLetterBox: View {
+    let color : Color
+    let text : String
+    var body: some View {
+        ZStack{
+            color
+            Text(text)
+                .font(.system(size: 90))
+                .fontWeight(.heavy)
+        }
+        .frame(width: 120, height: 120)
+    }
+}
 #Preview {
     ContentView()
 }
+
+
+
+
+
+//    let position = alphabet.index(alphabet.startIndex, offsetBy: counter)
+//    letter = String(alphabet[position])
+//    counter += 1
