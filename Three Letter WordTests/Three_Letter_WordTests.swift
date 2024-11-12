8cf3b6ecbdea8f079ff58df2494120322e52cfa1
Zane <zmatarieh@bsd220.org>
2024-11-12 09:45:05 -0600

    Initial Commit

---
+++ Three Letter WordTests/Three_Letter_WordTests.swift
@@ -0,0 +1,17 @@
+//
+//  Three_Letter_WordTests.swift
+//  Three Letter WordTests
+//
+//  Created by Zane Matarieh on 11/12/24.
+//
+
+import Testing
+@testable import Three_Letter_Word
+
+struct Three_Letter_WordTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
