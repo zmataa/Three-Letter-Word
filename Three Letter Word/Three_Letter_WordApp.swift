96cb3ac8265b5edad571b385b0839979fa7d6401
Zane <zmatarieh@bsd220.org>
2024-11-12 09:45:05 -0600

    Initial Commit

---
+++ Three Letter Word/Three_Letter_WordApp.swift
@@ -0,0 +1,17 @@
+//
+//  Three_Letter_WordApp.swift
+//  Three Letter Word
+//
+//  Created by Zane Matarieh on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Three_Letter_WordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
